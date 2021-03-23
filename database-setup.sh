c5e613522a37a28401610789f4fa351289323a12
Tom Moor <tom.moor@gmail.com>
2021-03-22 17:22:35 -0700

    Update database-setup.sh

--- database-setup.sh
+++ database-setup.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-sudo docker-compose run --rm outline yarn sequelize db:setup --env=production-ssl-disabled
+sudo docker-compose run --rm outline yarn sequelize db:create --env=production-ssl-disabled
 sudo docker-compose run --rm outline yarn sequelize db:migrate --env=production-ssl-disabled
